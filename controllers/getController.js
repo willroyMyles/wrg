353c26e1a365b41068ab4217625f76efffd5945f
Willroy <willroymyles@yahoo.com>
2019-06-18 15:49:44 -0500

    Implement board name from file

--- controllers/getController.js
+++ controllers/getController.js
@@ -1,5 +1,18 @@
 module.exports = async(app) => {
 
+    var Papa = require('papaparse');
+    var fs = require('fs');
+    const csv = fs.readFileSync('parts.txt', 'utf8');
+    var partsArr = [];
+    console.log(csv);
+    Papa.parse(csv, {
+        complete: function(results) {
+            partsArr = results.data;
+            console.log(partsArr)
+        }
+    });
+    app.parts = partsArr;
+
     app.get('/', (req, res) => {
         res.render('body', { data: false, login: false });
     });
@@ -19,7 +32,15 @@ module.exports = async(app) => {
     })
 
 
-
+    app.get('/engine&drivetrain', async(req, res) => {
+        var t = app.parts[2][0];
+        var opts = [];
+        for (let index = 1; index < app.parts[2].length; index++) {
+            const element = app.parts[2][index];
+            opts.push(element);
+        }
+        res.render('primary pages/primary template', { title: app.parts[2][0], links: opts })
+    })
 
 
 
