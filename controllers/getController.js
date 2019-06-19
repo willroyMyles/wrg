c222750a3aa3850382217ea231c20115cafb41b2
Willroy <willroymyles@yahoo.com>
2019-06-19 08:23:29 -0500

    set up routes for main pages

--- controllers/getController.js
+++ controllers/getController.js
@@ -4,11 +4,9 @@ module.exports = async(app) => {
     var fs = require('fs');
     const csv = fs.readFileSync('parts.txt', 'utf8');
     var partsArr = [];
-    console.log(csv);
     Papa.parse(csv, {
         complete: function(results) {
             partsArr = results.data;
-            console.log(partsArr)
         }
     });
     app.parts = partsArr;
@@ -31,8 +29,29 @@ module.exports = async(app) => {
         res.render('body', { data: arr });
     })
 
+    var routeText = [];
+    var r;
+    app.parts.forEach(element => {
+        for (let i = 0; i < element.length; i++) {
+            r = element[i].replace(/ /g, '%20');
+            routeText.push(r);
+        }
+    });
 
-    app.get('/engine&drivetrain', async(req, res) => {
+    routeText.forEach((val, index) => {
+        app.get('/' + val, (req, res) => {
+            var index = getIndexOfRoute(val);
+            var opts = [];
+            console.log(index);
+            for (let i = 1; i < app.parts[index.first].length; i++) {
+                const element = app.parts[index.first][i];
+                opts.push(element);
+            }
+            res.render('primary pages/primary template', { title: app.parts[index.first][index.second], links: opts })
+        })
+    })
+
+    app.get('/' + routeText, async(req, res) => {
         var t = app.parts[2][0];
         var opts = [];
         for (let index = 1; index < app.parts[2].length; index++) {
@@ -45,5 +64,27 @@ module.exports = async(app) => {
 
 
 
+    function getIndexOfRoute(route) {
+        var val = route.replace(/%20/g, ' ');
+        var first = 0;
+        var second = 0;
 
+        for (let i = 0; i < app.parts.length; i++) {
+            console.log(app.parts[i])
+            if (val == app.parts[i]) return { first, second };
+
+
+            for (let j = 0; j < app.parts[i].length; j++) {
+
+                const element = app.parts[i][j];
+                if (val == app.parts[i][j]) return { first, second };
+                second++;
+            }
+            first++;
+            second = 0;
+        }
+
+        return { first, second };
+
+    }
 }
